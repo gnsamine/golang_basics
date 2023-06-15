3b76d9bb87248ec5b23087a196b25c5bac8501e8
gnsamine <gunesamine0@gmail.com>
2023-06-15 12:35:48 +0300

    guessgame

--- loops/workshop1.go
+++ loops/workshop1.go
@@ -6,23 +6,36 @@ import "fmt"
 func Demo3() {
 	myNumber := 15
 	estimatedNumber := 1
+	guessAmount := 0
 
 	fmt.Println("keep a number in your mind between 10 to 30")
 	fmt.Scanln(&estimatedNumber)
+	guessAmount = guessAmount + 1
 
-	if estimatedNumber == myNumber {
-		fmt.Println("congratulation. Now you are master of reading mind.")
-
-	}
 	for estimatedNumber != myNumber {
 		if estimatedNumber < myNumber {
 			fmt.Println("make it bigger ;)")
 			fmt.Scanln(&estimatedNumber)
+			guessAmount = guessAmount + 1
 
 		} else {
 			fmt.Println("make it samller :D")
 			fmt.Scanln(&estimatedNumber)
+			guessAmount = guessAmount + 1
+
 		}
 	}
+
+	guessStanding := " "
+	if guessAmount > 0 && guessAmount <= 3 {
+		guessStanding = "mind reader"
+
+	} else if guessAmount > 3 && guessAmount <= 5 {
+		guessStanding = "good job"
+	} else {
+		guessStanding = "not good"
+	}
+
+	fmt.Printf("congratulations. %v guess : %v ", guessAmount, guessStanding)
 
 }
